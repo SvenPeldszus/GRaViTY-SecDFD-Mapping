4ba5282de2136e02c199b9f86679356d41a63b04
Sven Peldszus <speldszus@uni-koblenz.de>
2019-02-08 09:47:04 +0100

    Improved compare

--- implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/StringCompare.java
+++ implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/StringCompare.java
@@ -1,9 +1,31 @@
 package org.gravity.mapping;
 
+/**
+ * This class provides functionalities for finding similiar Strings
+ * 
+ * @author speldszus
+ *
+ */
 public class StringCompare {
 
+	/**
+	 * Compares the two given Strings and checks if they are similar
+	 * 
+	 * @param first The first String
+	 * @param second The second String
+	 * @return true, if the Strings are similar
+	 */
 	public static boolean compare(String first, String second) {
-		if(first.length() > second.length()) {
+		int length1 = first.length();
+		int length2 = second.length();
+		
+		// The length of the Stings should not be too different
+		if(Math.abs(length1 - length2) > Math.max(length1, length2)*0.5) {
+			return false;
+		}
+		
+		// Check if the longer String contains the shorter String
+		if(length1 > length2) {
 			return first.toLowerCase().contains(second.toLowerCase());
 		}
 		else {
