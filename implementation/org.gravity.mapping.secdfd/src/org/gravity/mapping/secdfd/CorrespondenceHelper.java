8b8eb27e23b555d88610150c38e5fa0929e09ed1
katjat <katja.tuma@cse.gu.se>
2019-04-05 16:27:29 +0200

    soreted list

--- implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/secdfd/CorrespondenceHelper.java
+++ implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/secdfd/CorrespondenceHelper.java
@@ -18,6 +18,7 @@ import org.gravity.mapping.secdfd.model.mapping.MappingFactory;
 import org.gravity.mapping.secdfd.model.mapping.MappingProcessDefinition;
 import org.gravity.mapping.secdfd.model.mapping.MappingProcessName;
 import org.gravity.mapping.secdfd.model.mapping.MappingProcessSignature;
+import org.gravity.mapping.secdfd.model.mapping.MappingRanking;
 import org.gravity.mapping.secdfd.views.MappingLabelProvider;
 import org.gravity.typegraph.basic.TAbstractType;
 import org.gravity.typegraph.basic.TMember;
@@ -67,6 +68,18 @@ public class CorrespondenceHelper {
 		mapping.getCorrespondences().add(corr);
 		addToMap(element, corr);
 		addToMap(member, corr);
+		Collection<AbstractCorrespondence> bonus_corr = getCorrespondence(member, element);
+		if (!bonus_corr.isEmpty()) {
+			AbstractCorrespondence ac = bonus_corr.iterator().next();
+			//increase the ranking
+			if(((MappingRanking) ac).getRanking() < 100) {
+				((MappingRanking) ac).setRanking(((MappingRanking) ac).getRanking()+15);
+				
+			}
+		} else if(bonus_corr.size()>1) {
+			//logg it
+			LOGGER.log(Level.INFO, "More than one corresponcende: " + MappingLabelProvider.prettyPrint(corr));
+		}
 		LOGGER.log(Level.INFO, "Create correspondence: " + MappingLabelProvider.prettyPrint(corr));
 		return corr;
 	}
@@ -89,6 +102,18 @@ public class CorrespondenceHelper {
 		mapping.getCorrespondences().add(corr);
 		addToMap(element, corr);
 		addToMap(signature, corr);
+		Collection<AbstractCorrespondence> bonus_corr = getCorrespondence(signature, element);
+		if (!bonus_corr.isEmpty()) {
+			AbstractCorrespondence ac = bonus_corr.iterator().next();
+			//increase the ranking
+			if(((MappingRanking) ac).getRanking() < 100) {
+				((MappingRanking) ac).setRanking(((MappingRanking) ac).getRanking()+15);
+				
+			}
+		} else if(bonus_corr.size()>1) {
+			//logg it
+			LOGGER.log(Level.INFO, "More than one corresponcende: " + MappingLabelProvider.prettyPrint(corr));
+		}
 		if (getCorrespondences(signature.getMethod()).isEmpty()) {
 			Method2Element parentCorr = createCorrespondence(signature.getMethod(), element, ranking);
 			corr.getDerived().add(parentCorr);
@@ -114,6 +139,18 @@ public class CorrespondenceHelper {
 		mapping.getCorrespondences().add(corr);
 		addToMap(element, corr);
 		addToMap(member, corr);
+		Collection<AbstractCorrespondence> bonus_corr = getCorrespondence(member, element);
+		if (!bonus_corr.isEmpty()) {
+			AbstractCorrespondence ac = bonus_corr.iterator().next();
+			//increase the ranking
+			if(((MappingRanking) ac).getRanking() < 100) {
+				((MappingRanking) ac).setRanking(((MappingRanking) ac).getRanking()+15);
+				
+			}
+		} else if(bonus_corr.size()>1) {
+			//logg it
+			LOGGER.log(Level.INFO, "More than one corresponcende: " + MappingLabelProvider.prettyPrint(corr));
+		}
 		if (getCorrespondences(member.getSignature()).isEmpty()) {
 			TMethodSignature signature = ((TMethodDefinition) member).getSignature();
 			corr.getDerived().add(createCorrespondence(signature, element, ranking, Collections.emptyList()));
@@ -138,6 +175,18 @@ public class CorrespondenceHelper {
 		mapping.getCorrespondences().add(corr);
 		addToMap(entity, corr);
 		addToMap(type, corr);
+		Collection<AbstractCorrespondence> bonus_corr = getCorrespondence(type, entity);
+		if (!bonus_corr.isEmpty()) {
+			AbstractCorrespondence ac = bonus_corr.iterator().next();
+			//increase the ranking
+			if(((MappingRanking) ac).getRanking() < 100) {
+				((MappingRanking) ac).setRanking(((MappingRanking) ac).getRanking()+15);
+				
+			}
+		} else if(bonus_corr.size()>1) {
+			//logg it
+			LOGGER.log(Level.INFO, "More than one corresponcende: " + MappingLabelProvider.prettyPrint(corr));
+		}
 		LOGGER.log(Level.INFO, "Create correspondence: " + MappingLabelProvider.prettyPrint(corr));
 		return corr;
 	}
--- implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/secdfd/views/MappingContentProvider.java
+++ implementation/org.gravity.mapping.secdfd/src/org/gravity/mapping/secdfd/views/MappingContentProvider.java
@@ -1,16 +1,48 @@
 package org.gravity.mapping.secdfd.views;
 
 import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.gravity.mapping.secdfd.model.mapping.AbstractMappingDerived;
 import org.gravity.mapping.secdfd.model.mapping.Mapping;
+import org.gravity.mapping.secdfd.model.mapping.MappingEntityType;
+import org.gravity.mapping.secdfd.model.mapping.MappingProcessName;
+import org.gravity.mapping.secdfd.model.mapping.MappingRanking;
 import org.moflon.tgg.runtime.AbstractCorrespondence;
 import org.moflon.tgg.runtime.CorrespondenceModel;
 
 public class MappingContentProvider implements ITreeContentProvider {
+
+	private final class compareByRanking implements Comparator<Object> {
+		@Override
+		public int compare(Object a, Object b) {
+			if (a instanceof MappingProcessName || a instanceof MappingEntityType || a instanceof AbstractMappingDerived) {
+				//a has a ranking
+				if (a instanceof MappingProcessName || a instanceof MappingEntityType || a instanceof AbstractMappingDerived) {
+					//b has a ranking
+					if (((MappingRanking) a).getRanking()>((MappingRanking) b).getRanking()) return -1;
+					else if ( ((MappingRanking) a).getRanking()==((MappingRanking) b).getRanking() ) {
+						return 0;
+					}else return 1;
+				}else {
+					//b has no ranking, a is smaller
+					return 1;
+				}
+			}else {
+				//a has no ranking, a is greater
+				return -1;
+			}
+		}
+	}
 
 	@Override
 	public Object[] getElements(Object inputElement) {
@@ -20,6 +52,23 @@ public class MappingContentProvider implements ITreeContentProvider {
 		return null;
 	}
 
+	public boolean firstHigherRank(Object a, Object b) {
+		if (a instanceof MappingProcessName || a instanceof MappingEntityType || a instanceof AbstractMappingDerived) {
+			//a has a ranking
+			if (a instanceof MappingProcessName || a instanceof MappingEntityType || a instanceof AbstractMappingDerived) {
+				//b has a ranking
+				return ((MappingRanking) a).getRanking()>((MappingRanking) b).getRanking();
+			}else {
+				//b has no ranking
+				return false;
+			}
+		}else {
+			//a has no ranking
+			return true;
+		}
+	}
+
+	
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof Mapping) {
@@ -31,9 +80,17 @@ public class MappingContentProvider implements ITreeContentProvider {
 					new AbstractMap.SimpleEntry<String, Collection<AbstractCorrespondence>>("accepted",
 							((Mapping) parentElement).getAccepted()) };
 		} else if (parentElement instanceof CorrespondenceModel) {
-			return ((CorrespondenceModel) parentElement).getCorrespondences().toArray();
+			//order by ranking
+			Object[] to_sort = ((CorrespondenceModel) parentElement).getCorrespondences().toArray();
+			ArrayList<Object> list = new ArrayList<Object>(Arrays.asList(to_sort));
+			list.sort(new compareByRanking());
+			return list.toArray();
+			//((CorrespondenceModel) parentElement).getCorrespondences().toArray();
 		} else if (parentElement instanceof Collection) {
-			return ((Collection<?>) parentElement).toArray();
+			//order by ranking
+			ArrayList<Object> list = (ArrayList<Object>) new ArrayList<Object>(Arrays.asList(((Collection<?>) parentElement).toArray()));
+			list.sort(new compareByRanking());
+			return list.toArray();
 		} else if (parentElement instanceof Stream) {
 			return ((Stream<?>) parentElement).toArray();
 		}
