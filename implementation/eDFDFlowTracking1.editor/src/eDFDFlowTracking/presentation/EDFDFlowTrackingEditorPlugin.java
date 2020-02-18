6e12c4618108ec69bd871361c4cfb4c7a65e4654
Sven Peldszus <speldszus@uni-koblenz.de>
2020-02-18 17:12:18 +0100

    Migrated Sirius visualization

--- implementation/eDFDFlowTracking1.editor/src/eDFDFlowTracking/presentation/EDFDFlowTrackingEditorPlugin.java
+++ implementation/eDFDFlowTracking1.editor/src/eDFDFlowTracking/presentation/EDFDFlowTrackingEditorPlugin.java
@@ -2,51 +2,52 @@
  */
 package eDFDFlowTracking.presentation;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.emf.common.EMFPlugin;
 
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
 
 import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.sirius.business.api.componentization.ViewpointRegistry;
+import org.eclipse.sirius.viewpoint.description.Viewpoint;
+import org.osgi.framework.BundleContext;
 
 /**
- * This is the central singleton for the EDFDFlowTracking editor plugin.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the central singleton for the EDFDFlowTracking editor plugin. <!--
+ * begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 public final class EDFDFlowTrackingEditorPlugin extends EMFPlugin {
 	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public static final EDFDFlowTrackingEditorPlugin INSTANCE = new EDFDFlowTrackingEditorPlugin();
-	
+
 	/**
-	 * Keep track of the singleton.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	private static Implementation plugin;
 
 	/**
-	 * Create the instance.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public EDFDFlowTrackingEditorPlugin() {
-		super
-			(new ResourceLocator [] {
-			});
+		super(new ResourceLocator[] {});
 	}
 
 	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the singleton instance.
 	 * @generated
 	 */
@@ -54,37 +55,85 @@ public final class EDFDFlowTrackingEditorPlugin extends EMFPlugin {
 	public ResourceLocator getPluginResourceLocator() {
 		return plugin;
 	}
-	
+
 	/**
-	 * Returns the singleton instance of the Eclipse plugin.
-	 * <!-- begin-user-doc -->
+	 * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @return the singleton instance.
 	 * @generated
 	 */
 	public static Implementation getPlugin() {
 		return plugin;
 	}
-	
+
 	/**
-	 * The actual implementation of the Eclipse <b>Plugin</b>.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The actual implementation of the Eclipse <b>Plugin</b>. <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public static class Implementation extends EclipseUIPlugin {
 		/**
-		 * Creates an instance.
-		 * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
+		 * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
+		 * 
 		 * @generated
 		 */
 		public Implementation() {
 			super();
-	
+
 			// Remember the static instance.
 			//
 			plugin = this;
+		}
+
+		// The plug-in ID
+		public static final String PLUGIN_ID = "eDFDFlowTracking1.editor";
+
+		// The shared instance
+		private static Implementation plugin;
+
+		private static Set<Viewpoint> viewpoints;
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+		 * BundleContext)
+		 */
+		public void start(BundleContext context) throws Exception {
+			super.start(context);
+			plugin = this;
+			viewpoints = new HashSet<>();
+			viewpoints.addAll(
+					ViewpointRegistry.getInstance().registerFromPlugin(PLUGIN_ID + "/description/project.odesign"));
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+		 */
+		public void stop(BundleContext context) throws Exception {
+			plugin = null;
+			if (viewpoints != null) {
+				for (final Viewpoint viewpoint : viewpoints) {
+					ViewpointRegistry.getInstance().disposeFromPlugin(viewpoint);
+				}
+				viewpoints.clear();
+				viewpoints = null;
+			}
+			super.stop(context);
+		}
+
+		/**
+		 * Returns the shared instance
+		 * 
+		 * @return the shared instance
+		 */
+		public static Implementation getDefault() {
+			return plugin;
 		}
 	}
 
